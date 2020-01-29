ea2b4729def88ff2dcc3a503b445c73f46091620
jk977 <jdk64@comcast.net>
2020-01-29 01:05:25 -0800

    Adding failing dict test

--- tests/test_dict.c
+++ tests/test_dict.c
@@ -10,8 +10,40 @@ bool test_init(void) {
     return true;
 }
 
+bool test_set(void) {
+    struct dict* dict = dict_init(NULL);
+
+    dict_set(dict, "foo", 1);
+    dict_set(dict, "bar", 2);
+    dict_set(dict, "baz", 3);
+
+    dict_destroy(dict);
+
+    return true;
+}
+
+bool test_contains(void) {
+    bool success = true;
+    struct dict* dict = dict_init(NULL);
+
+    dict_set(dict, "foo", 1);
+    dict_set(dict, "bar", 2);
+    dict_set(dict, "baz", 3);
+
+    success = success
+        && dict_contains(dict, "foo")
+        && dict_contains(dict, "bar")
+        && dict_contains(dict, "baz");
+
+    dict_destroy(dict);
+
+    return success;
+}
+
 int main(void) {
-    bool success = test_init();
+    bool success = test_init()
+        && test_set()
+        && test_contains();
 
     return success ?
         EXIT_SUCCESS :
