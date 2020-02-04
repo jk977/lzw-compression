7f91021837e2ecc2aa5fa567bb6033a08f10fff5
jk977 <jdk64@comcast.net>
2020-02-03 18:25:55 -0800

    Improving outstream documentation, readability

--- src/outstream.c
+++ src/outstream.c
@@ -15,6 +15,9 @@ struct outstream {
     uint32_t outcount;
 };
 
+/*
+ * outs_init: Initialize an output bitstream on the heap.
+ */
 struct outstream* outs_init(void (*write_byte)(unsigned char c, void* context))
 {
     struct outstream* outs = malloc(sizeof(*outs));
@@ -33,11 +36,20 @@ struct outstream* outs_init(void (*write_byte)(unsigned char c, void* context))
     return outs;
 }
 
+/*
+ * outs_destroy: Free the structure created by outs_init().
+ */
 void outs_destroy(struct outstream* outs)
 {
     free(outs);
 }
 
+/*
+ * outs_write_bits: Write the given bits.
+ *                  If any bits are stored in the buffer, they are added to
+ *                  the given bits before writing. Any amount of bits under
+ *                  CHAR_BIT are kept in the buffer for the next write.
+ */
 void outs_write_bits(struct outstream* outs, int bits, size_t bit_count)
 {
     size_t pending_bits = bit_count + outs->bufsize;
@@ -46,12 +58,17 @@ void outs_write_bits(struct outstream* outs, int bits, size_t bit_count)
         // size of the empty bits in buffer
         size_t const lower_size = CHAR_BIT - outs->bufsize;
 
-        // ensure that outs->buffer and bits don't contain garbage bits
+        // ensure that outs->buffer and bits don't contain garbage by
+        // zeroing unimportant bits
         unsigned char const upper_mask = (~0 << lower_size);
-        unsigned char const lower_mask = (~0 >> lower_size);
+        unsigned char const lower_mask = ~upper_mask;
 
+        // put the old buffer bits into the upper part of the output,
+        // and the parameter bits into the lower part, as specified by
+        // the assignment description
         outs->buffer = (outs->buffer & upper_mask) | (bits & lower_mask);
         outs->bufsize = CHAR_BIT;
+
         outs_flush(outs);
 
         // update information about the remaining bits.
@@ -68,6 +85,11 @@ void outs_write_bits(struct outstream* outs, int bits, size_t bit_count)
     outs->bufsize = pending_bits;
 }
 
+/*
+ * outs_flush: Flush the buffer of the output bitstream.
+ *             Since outs_write_bits() ensures the buffer is properly zeroed,
+ *             this function does not check for garbage bits in the buffer.
+ */
 void outs_flush(struct outstream* outs)
 {
     if (outs->bufsize == 0) {
---
+++ tests/test_outstream.c
@@ -0,0 +1,9 @@
+#include "outstream.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    return EXIT_SUCCESS;
+}
