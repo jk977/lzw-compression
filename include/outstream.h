4b409cfd3be36e773e9408f108d80e92af7a4807
jk977 <jdk64@comcast.net>
2020-02-03 17:31:39 -0800

    Beginning outstream write implementation

--- include/outstream.h
+++ include/outstream.h
@@ -6,7 +6,7 @@ struct outstream;
 struct outstream* outs_init(void (*write_byte)(unsigned char c, void* context));
 void outs_destroy(struct outstream* outs);
 
-void outs_write_bits(struct outstream* outs, long int bits, size_t bit_count);
+void outs_write_bits(struct outstream* outs, int bits, size_t bit_count);
 void outs_flush(struct outstream* outs);
 
 #endif // OUTSTREAM_H_
--- src/outstream.c
+++ src/outstream.c
@@ -1,18 +1,22 @@
 #include "outstream.h"
 
 #include <stdlib.h>
+#include <stdint.h>
+
+#include <limits.h>
 
 struct outstream {
     void (*write)(unsigned char, void*);
     void* context;
 
-    unsigned int outcount;
+    uint32_t outcount;
 
-    long int buffer;
-    unsigned int bufsize;
+    uint32_t buffer;
+    uint8_t bufsize;
 };
 
-struct outstream* outs_init(void (*write_byte)(unsigned char c, void* context)) {
+struct outstream* outs_init(void (*write_byte)(unsigned char c, void* context))
+{
     struct outstream* outs = malloc(sizeof(*outs));
 
     if (outs == NULL) {
@@ -29,12 +33,45 @@ struct outstream* outs_init(void (*write_byte)(unsigned char c, void* context)) 
     return outs;
 }
 
-void outs_destroy(struct outstream* outs) {
+void outs_destroy(struct outstream* outs)
+{
     free(outs);
 }
 
-void outs_write_bits(struct outstream* outs, long int bits, size_t bit_count) {
+void outs_write_bits(struct outstream* outs, int bits, size_t bit_count)
+{
+    size_t pending_bits = bit_count + outs->bufsize;
+
+    while (pending_bits >= CHAR_BIT) {
+        if (pending_bits > CHAR_BIT) {
+            // write the next byte and store the remainder
+        } else {
+            // write a single byte and don't store anything
+        }
+
+        pending_bits -= CHAR_BIT;
+    }
+
+    if (pending_bits >= 0) {
+        // store remaining bits in buffer
+    }
+
+    outs->bufsize += bit_count;
+    outs->bufsize %= CHAR_BIT;
 }
 
-void outs_flush(struct outstream* outs) {
+void outs_flush(struct outstream* outs)
+{
+    if (outs->bufsize == 0) {
+        return;
+    }
+
+    // zero out garbage bits from buffer
+    uint32_t mask = (~0 >> outs->bufsize);
+    outs->buffer &= mask;
+
+    (outs->write)(outs->buffer);
+
+    outs->bufsize = 0;
+    ++outs->outcount;
 }
