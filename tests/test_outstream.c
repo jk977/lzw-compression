5c46708d41ee692c9b7de3d5e77d3132ef95222b
jk977 <jdk64@comcast.net>
2020-02-05 13:59:22 -0800

    Exposing bugs in outstream implementation

--- tests/test_outstream.c
+++ tests/test_outstream.c
@@ -8,6 +8,9 @@
 #include <limits.h>
 #include <string.h>
 
+#define FOREACH(i, ary) \
+    for (size_t i = 0; i < sizeof(ary) / sizeof((ary)[0]); ++i)
+
 static char const* default_file = "foo.txt";
 
 void write_file(unsigned char c, void* context)
@@ -18,12 +21,11 @@ void write_file(unsigned char c, void* context)
 
 void test_large(struct outstream* outs)
 {
-    uint16_t content[] = { 0b0111101110110111, 0b1100000011101111 };
+    uint16_t content[] = { 0b1011011101111011, 0b1110111111000000 };
     size_t const write_size = 16;
 
     // expected write: 10110111 01111011 11101111 11000000
-    // least-significant byte is written first, then most significant byte
-    for (size_t i = 0; i < sizeof(content) / sizeof(content[0]); ++i) {
+    FOREACH (i, content) {
         outs_write_bits(outs, content[i], write_size);
     }
 }
@@ -42,7 +44,7 @@ void test_bits(struct outstream* outs)
     unsigned char content[] = { 'a', '\n' };
     size_t write_size = 2;
 
-    for (size_t i = 0; i < sizeof(content) / sizeof(content[0]); ++i) {
+    FOREACH(i, content) {
         unsigned char current = content[i];
         size_t written = 0;
 
@@ -60,6 +62,23 @@ void test_bits(struct outstream* outs)
 
         ++write_size;
     }
+}
+
+void test_vary(struct outstream* outs)
+{
+    unsigned char content[] = {
+        0b1, 0b10, 0b100, 0b1000, 0b10000, 0b100000
+    };
+
+    size_t write_size = 1;
+
+    // expected write: 11010010 00100001 00000000
+    FOREACH(i, content) {
+        outs_write_bits(outs, content[i], write_size);
+        ++write_size;
+    }
+
+    outs_flush(outs);
 }
 
 static void test_file(char const* path,
@@ -83,6 +102,7 @@ int main(void)
     test_file("a.txt", test_large);
     test_file("b.txt", test_bytes);
     test_file("c.txt", test_bits);
+    test_file("d.txt", test_vary);
 
     return EXIT_SUCCESS;
 }
